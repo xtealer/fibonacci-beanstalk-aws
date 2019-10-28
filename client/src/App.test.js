5a475d84505d652df53d6a040b78317b1dfcdbec
Humberto E. De Leon <humberto0118@gmail.com>
2019-10-28 10:57:26 -0500

    fix client project.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,4 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-it("renders without crashing", () => {});
+it("renders without crashing", () => {
+  console.log("it works");
+});
